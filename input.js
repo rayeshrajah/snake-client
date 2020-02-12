829b11be5d4c3dacb44d3f9a5c45170ec38b27bc
Rayesh <rayeshrajah@gmail.com>
2020-02-12 19:48:02 +0000

    Edited input and play files

--- input.js
+++ input.js
@@ -1,13 +1,28 @@
-const setupInput = function() {
+let connections;
+const handleUserInput = ('data', (input) =>{
+    if(input === '\u0003'){
+        process.exit();
+    }
+    else if(input === 'w'){
+        connections.write('Move: up');
+    }
+    else if(input === 'a'){
+        connections.write('Move: left');
+    }
+    else if(input === 'd'){
+        connections.write('Move: right');
+    }else if(input === 's'){
+        connections.write('Move: down')
+    }
+  })
+const setupInput = function(conn) {
+    connections = conn;
     const stdin = process.stdin;
     stdin.setRawMode(true);
     stdin.setEncoding('utf8');
     stdin.resume();
-    const handleUserInput = (data, () =>{
-    if(data === '\u0003'){
-        process.exit();
-    }
-  })
+    stdin.on('data', handleUserInput);
+    
     return stdin;
   }
 
